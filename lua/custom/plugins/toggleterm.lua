a916e457507d2bba0bde0b3cee4acccb7bcf93cc
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2024-05-24 14:32:59 -0500

    add multiple terms

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,7 +1,15 @@
 return {
   'akinsho/toggleterm.nvim',
   opts = {
-    open_mapping = [[<C-/>]],
+    open_mapping = { [[<C-/>]] },
     direction = 'float',
+  },
+  keys = {
+    { '<C-\\>', '<cmd>1ToggleTerm direction=float<cr>' },
+    { '<C-1>', '<cmd>2ToggleTerm direction=horizontal<cr>' },
+    { '<C-2>', '<cmd>3ToggleTerm direction=horizontal<cr>' },
+    { '<C-3>', '<cmd>4ToggleTerm direction=horizontal<cr>' },
+    { '<C-4>', '<cmd>5ToggleTerm direction=horizontal<cr>' },
+    { '<C-5>', '<cmd>6ToggleTerm direction=horizontal<cr>' },
   },
 }
